18fc92eb8a6267c975b1c99fe95ea67184942afc
aishwaryakurakula2511@gmail.com <aishwaryakurakula2511@gmail.com>
2026-03-27 09:59:54 +0530

    fix: update axios baseURL to use VITE_API_URL for production

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -4,13 +4,8 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "/api",
+      : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
 export default axiosInstance;
-
-
-
-
-
