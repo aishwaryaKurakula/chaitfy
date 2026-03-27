f2d44c5bf086046966f772ec1f155897de1effd3
aishwaryakurakula2511@gmail.com <aishwaryakurakula2511@gmail.com>
2026-03-27 20:48:24 +0530

    Fix auth handling and Netlify routing

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -43,4 +43,15 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default axiosInstance;
